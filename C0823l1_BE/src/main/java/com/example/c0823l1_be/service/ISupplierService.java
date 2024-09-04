fedecad09e1d45932ebda854cba2123e1a0fd89f
XuanBaoJunior <nguyenxuanbao031202@gmail.com>
2024-09-04 18:55:42 +0700

    test up

--- C0823l1_BE/src/main/java/com/example/c0823l1_be/service/ISupplierService.java
+++ C0823l1_BE/src/main/java/com/example/c0823l1_be/service/ISupplierService.java
@@ -1,2 +1,19 @@
-package com.example.c0823l1_be.service;public interface ISupplierService {
+package com.example.c0823l1_be.service;
+
+import com.example.c0823l1_be.entity.Supplier;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface ISupplierService {
+    Page<Supplier> findAll(Pageable pageable);
+
+    Page<Supplier> searchSuppliers(String search, Pageable pageable);
+
+    void deleteByIds(List<Integer> ids);
+
+    Page<Supplier> searchByName(String name, Pageable pageable);
+
+    List<String> findAllSupplierNames();
 }
--- C0823l1_BE/src/main/java/com/example/c0823l1_be/service/imp/SupplierService.java
+++ C0823l1_BE/src/main/java/com/example/c0823l1_be/service/imp/SupplierService.java
@@ -1,2 +1,47 @@
-package com.example.c0823l1_be.service.imp;public class SupplierService {
+package com.example.c0823l1_be.service.imp;
+
+import com.example.c0823l1_be.entity.Supplier;
+import com.example.c0823l1_be.repository.ISupplierRepository;
+import com.example.c0823l1_be.service.ISupplierService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class SupplierService implements ISupplierService {
+    @Autowired
+    private ISupplierRepository iSupplierRepository;
+
+    @Override
+    public Page<Supplier> findAll(Pageable pageable) {
+        return iSupplierRepository.findAll(pageable);
+    }
+
+    @Override
+    public Page<Supplier> searchSuppliers(String search, Pageable pageable) {
+        return iSupplierRepository.searchSuppliers(search, pageable);
+    }
+
+
+    public void deleteByIds(List<Integer> ids) {
+        iSupplierRepository.deleteAllById(ids);
+    }
+
+    @Override
+    public Page<Supplier> searchByName(String name, Pageable pageable) {
+        return iSupplierRepository.findByName(name, pageable);
+    }
+
+    @Override
+    public List<String> findAllSupplierNames() {
+        return iSupplierRepository.findAll()
+                .stream()
+                .map(Supplier::getName)
+                .collect(Collectors.toList());
+    }
+
 }
